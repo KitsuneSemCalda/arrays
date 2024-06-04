aa6f20e7a5be10edd5e066f06f753d7231ff6dd9
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-06-04 09:13:43 -0300

    test (test clone): now test clone test if the new struct is the same than previous

--- tests/arrays/clone_test.go
+++ tests/arrays/clone_test.go
@@ -1,18 +1,32 @@
 package arrays_test
 
 import (
-	"github.com/w1tchCrafter/arrays/pkg/arrays"
 	"testing"
+
+	"github.com/w1tchCrafter/arrays/pkg/arrays"
 )
 
-func TestCLone(t *testing.T) {
+func TestClone(t *testing.T) {
 	arr := arrays.New(1, 2, 3, 4, 5)
+
+	// Create a clone of the array
 	slc := arr.Clone()
 
+	// Append additional elements to the original array
 	arr.Push(6, 7, 8)
 
-	// slc should not be affected
-	if len(slc) == arr.Len() {
-		t.Fail()
+	// Check if the length of the clone remains unchanged
+	if slc.Len() == arr.Len() {
+		t.Errorf("Expected length of clone to be different than %d but the length is the same", arr.Len())
+	}
+
+	// Check if the content of the clone remains unchanged
+	for i := 0; i < slc.Len(); i++ {
+		slcValue, _ := slc.At(i)
+		arrValue, _ := slc.At(i)
+
+		if slcValue != arrValue {
+			t.Errorf("Content of the clone differs from the original array at index %d", i)
+		}
 	}
 }
